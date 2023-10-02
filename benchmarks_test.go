3bab4dd7cb9a3b1f5765e178791b614524071406
Jared Wasinger <j-wasinger@hotmail.com>
2023-10-02 20:28:00 +0700

    some benchmarks

---
+++ benchmarks_test.go
@@ -0,0 +1,50 @@
+package slog_benchmarks
+
+import (
+	"io"
+	//"os"
+	"testing"
+
+	"github.com/ethereum/go-ethereum/log"
+)
+
+func BenchmarkJson(b *testing.B) {
+	log.Root().SetHandler(log.WrapHandler(log.MinLvl, log.StreamHandler(io.Discard, log.JSONFormat())))
+
+	for i := 0; i < b.N; i++ {
+		log.Info("a message", "foo", "bar", "baz", "bat")		
+	}
+}
+
+func BenchmarkGloggerNotEnabled(b *testing.B) {
+	glogHandler := log.NewGlogHandler(log.StreamHandler(io.Discard, log.TerminalFormat(false)))
+	glogHandler.Verbosity(log.LvlError)
+	log.Root().SetHandler(glogHandler)
+
+	for i := 0; i < b.N; i++ {
+		log.Info("foo", "bar", "baz", "bat")		
+	}
+	
+}
+
+func BenchmarkGloggerEnabledDiscardHandler(b *testing.B) {
+	glogHandler := log.NewGlogHandler(log.DiscardHandler())
+	glogHandler.Verbosity(log.LvlInfo)
+	log.Root().SetHandler(glogHandler)
+
+	for i := 0; i < b.N; i++ {
+		log.Info("foo", "bar", "baz", "bat")		
+	}
+	
+}
+
+func BenchmarkGloggerEnabledStreamHandler(b *testing.B) {
+	glogHandler := log.NewGlogHandler(log.StreamHandler(io.Discard, log.TerminalFormat(false)))
+	glogHandler.Verbosity(log.LvlInfo)
+	log.Root().SetHandler(glogHandler)
+
+	for i := 0; i < b.N; i++ {
+		log.Info("foo", "bar", "baz", "bat")		
+	}
+	
+}
